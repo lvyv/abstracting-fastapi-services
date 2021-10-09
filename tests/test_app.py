bb0d3bbc49d410666c47b7036a7a516df8644ef7
lvyv <26896225@qq.com>
2021-10-09 12:41:26 +0800

    initially commit

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,5 +1,64 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# Copyright 2021 The CASICloud Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ==============================================================================
+# pylint: disable=invalid-name
+# pylint: disable=missing-docstring
+
+"""
+=========================
+unit test module
+=========================
+
+测试模型调度核心模块的入口主进程.
+"""
+
+# Author: Awen <26896225@qq.com>
+# License: MIT
+
+import unittest
+import uvicorn
+from app.main import app as app_
 from app import __version__
 
 
 def test_version():
     assert __version__ == "0.1.0"
+
+
+class TestMain(unittest.TestCase):
+    """
+    Tests for `v2v` entrypoint.
+    本测试案例启动整个v2v程序（前提需要启动Mock提供仿真接口，并启动obs的rtsp服务器）
+    访问运行本案例的URL：
+    https://IP:29080/docs，执行POST /subprocess，发送start/stop命令启停视频识别流水线。
+    注意
+    """
+    def setUp(self):
+        """Set up test fixtures, if any."""
+
+    def tearDown(self):
+        """Tear down test fixtures, if any."""
+
+    def test_MainContext(self):
+        """Test core.main.MainContext."""
+        uvicorn.run(app_,  # noqa 标准用法
+                host="0.0.0.0",
+                port=8081)
+
+
+if __name__ == "__main__":
+    unittest.main()
